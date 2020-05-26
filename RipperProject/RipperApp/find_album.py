1592e4f154d6b3332be3b9310a2b6fce04b13f78
austin <austinpearce15@yahoo.com>
2020-05-26 18:56:25 -0500

    Altered how to test for apostrophe errors with Spotify's API

--- RipperProject/RipperApp/find_album.py
+++ RipperProject/RipperApp/find_album.py
@@ -4,15 +4,14 @@ from .spotify_info import spotify
 def get_album(artist_name, title):
     sp = spotify()
 
-    # Remove apostrophes if applicable
-    artist_name = artist_name.replace("'", "")
-    title = title.replace("'", "")
-
     results = sp.search(q='artist:' + artist_name + ' track:' + title, type='track')
 
     # Check if found in Spotify's database
     if len(results['tracks']['items']) == 0:
-        return "Invalid", "Invalid"
+        # Try title without apostrophes
+        results = sp.search(q='artist:' + artist_name + ' track:' + title.replace("'", ""), type='track')
+        if len(results['tracks']['items']) == 0:
+            return "Invalid", "Invalid"
 
     cover = results['tracks']['items'][0]['album']['images'][0]['url']
     album_name = results['tracks']['items'][0]['album']['name']
